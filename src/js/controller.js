4f37f8424da086092d4386304f754afaad59ee92
Stefan Stankovic <stankovicnstefan@gmail.com>
2022-02-03 18:56:38 -0600

    add fetch, and recipe object

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,2 +1,42 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+const recipeContainer = document.querySelector(".recipe");
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} seconds.`));
+    }, s * 1000);
+  });
+};
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      //"https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+
+      " https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc7e"
+    );
+
+    const data = await res.json();
+    if (!res.ok)
+      throw new Error(`${data.message} ${res.statusText}: ${res.status}`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipe();
