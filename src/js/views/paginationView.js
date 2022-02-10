2a683c0df869e4eea4544c6ccfb9c3dada3d9345
stankovics <stankovicnstefan@gmail.com>
2022-02-10 17:35:03 -0600

    generateMarkup logic for pagination

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -4,15 +4,26 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkup() {
+    const curPage = this._data.page;
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
     console.log(numPages);
 
     // Page 1, and there are other pages
+    if (curPage === 1 && numPages > 1) {
+    }
     // Page 1, and there are NO other pages
+    if (curPage === 1 && numPages === 1) {
+    }
     // Last page
+    if (curPage === numPages && numPages > 1) {
+    }
     //Other page
+    if (curPage < numPages) {
+    }
+    // Page 1, and there are NO other pages
+    return '';
   }
 }
 export default new PaginationView();
