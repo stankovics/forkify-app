e081fd5f6560bef91f35ecc73c42e31dc332a7b4
stankovics <stankovicnstefan@gmail.com>
2022-02-10 17:44:12 -0600

    generateMarkup logic for pagination

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -12,15 +12,41 @@ class PaginationView extends View {
 
     // Page 1, and there are other pages
     if (curPage === 1 && numPages > 1) {
-    }
-    // Page 1, and there are NO other pages
-    if (curPage === 1 && numPages === 1) {
+      return `
+        <button class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
     }
     // Last page
     if (curPage === numPages && numPages > 1) {
+      return `      
+       <button class="btn--inline pagination__btn--prev">
+         <svg class="search__icon">
+           <use href="${icons}#icon-arrow-left"></use>
+         </svg>
+         <span>Page ${curPage - 1}</span>
+       </button>`;
     }
     //Other page
     if (curPage < numPages) {
+      return `      
+       <button class="btn--inline pagination__btn--prev">
+         <svg class="search__icon">
+           <use href="${icons}#icon-arrow-left"></use>
+         </svg>
+         <span>Page ${curPage - 1}</span>
+       </button>
+       <button class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+       `;
     }
     // Page 1, and there are NO other pages
     return '';
