a26af5a2c70cc6f68904bd8a1ba24d2a24fd2510
stankovics <stankovicnstefan@gmail.com>
2022-02-19 12:49:45 -0600

    storing bookmarks

--- src/js/model.js
+++ src/js/model.js
@@ -71,12 +71,17 @@ export const updateServings = function (newServings) {
   // updating servings in the state
   state.recipe.servings = newServings;
 };
+const persistBookmarks = function () {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
+
 export const addBookmark = function (recipe) {
   // Add bookmark
   state.bookmarks.push(recipe);
 
   // Mark current recipe as bookmark
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+  persistBookmarks();
 };
 export const deleteBookmark = function (id) {
   // Delete bookmark
