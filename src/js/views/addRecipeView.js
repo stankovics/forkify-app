6a177be4845589b338b01f5882b84ca27ddd52d4
stankovics <stankovicnstefan@gmail.com>
2022-02-20 19:19:47 -0600

    addRecipeView.js & uploading a new recipe

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,8 @@
+import View from './View';
+import icons from 'url:../../img/icons.svg';
+class addRecipeView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {}
+}
+export default new addRecipeView();
