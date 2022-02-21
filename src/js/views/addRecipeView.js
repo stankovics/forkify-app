94ced1101a4d6aa939a5473c2bfdd77183251db1
stankovics <stankovicnstefan@gmail.com>
2022-02-20 19:25:56 -0600

    addHandlerShowWindow()

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,8 +1,18 @@
 import View from './View';
 import icons from 'url:../../img/icons.svg';
 class addRecipeView extends View {
-  _parentElement = document.querySelector('.pagination');
+  _parentElement = document.querySelector('.upload');
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
 
+  addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', function () {
+      this._overlay.classList.toggle('hidden');
+      this._window.classList.toggle('hidden');
+    });
+  }
   _generateMarkup() {}
 }
 export default new addRecipeView();
